afa7311098519e182e1f1c5ba6de4cee891f6099
nukulele <zonough@gmail.com>
2022-06-04 22:53:34 +0400

    copy dir

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,32 @@
+import { copyFile, mkdir, readdir } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const copy = async () => {
-    // Write your code here 
+  readdir(__dirname + "/files", (err, files) => {
+    if (err) {
+      throw new Error("FS operation failed");
+    }
+    if (files) {
+      mkdir(__dirname + "/files_copy", (err) => {
+        if (err) {
+          throw new Error("FS operation failed");
+        } else {
+          files.forEach((file) => {
+            copyFile(
+              __dirname + "/files/" + file,
+              __dirname + "/files_copy/" + file,
+              (err) => {
+                if (err) throw new Error(err.message);
+              }
+            );
+          });
+        }
+      });
+    }
+  });
 };
+
+copy();
