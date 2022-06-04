bddcc7f6396411be875d6bd04262694ff71bf997
nukulele <zonough@gmail.com>
2022-06-04 23:06:42 +0400

    delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,13 @@
+import { rm } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const remove = async () => {
-    // Write your code here 
+  rm(__dirname + "/files/fileToRemove.txt", (err) => {
+    if (err) throw new Error("FS operation failed");
+  });
 };
+
+remove();
