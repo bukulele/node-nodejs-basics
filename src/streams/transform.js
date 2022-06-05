f161c874bcf22097934d021dc8d0605d4efc4ec1
nukulele <zonough@gmail.com>
2022-06-05 19:44:06 +0400

    transform stream

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,16 @@
+import { Transform } from "stream";
+import { stdin, stdout } from "process";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 export const transform = async () => {
-    // Write your code here 
+  const reverse = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, Array.from(chunk.toString()).reverse().join(""));
+    },
+  });
+
+  stdin.pipe(reverse).pipe(stdout);
 };
+
+transform();
